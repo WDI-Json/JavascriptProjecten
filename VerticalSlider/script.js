0d465484f3a08b3272b620f8c97ea5ed1d63ff05
Wouter Dijks <wutu@Wouters-MacBook-Pro.local>
2021-08-28 23:41:47 +0200

    sliderproject in de basis af volgens tutorial

--- VerticalSlider/script.js
+++ VerticalSlider/script.js
@@ -7,7 +7,7 @@ const slidesLength = slideRight.querySelectorAll('div').length
 
 let activeSlideIndex = 0
 
-slideLeft.style.top = `-${(slidesLength - 1)* 100}vh`
+slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`
 
 upButton.addEventListener('click', () => changeSlide('up'))
 downButton.addEventListener('click', () => changeSlide('down'))
@@ -16,10 +16,16 @@ const changeSlide = (direction) => {
     const sliderHeight = sliderContainer.clientHeight
     if (direction == 'up') {
         activeSlideIndex++
-        if(activeSlideIndex > slidesLenght - 1) {
+        if (activeSlideIndex > slidesLength - 1) {
             activeSlideIndex = 0
+        }
+    } else if (direction == 'down') {
+        activeSlideIndex--
+        if (activeSlideIndex < 0) {
+            activeSlideIndex = slidesLength - 1
         }
     }
 
     slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`
+    slideLeft.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`
 }
