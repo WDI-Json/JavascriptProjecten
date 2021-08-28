13f929bfd4a51a70d919f6b9af5592405e6df12b
Wouter Dijks <wutu@Wouters-MacBook-Pro.local>
2021-08-28 23:27:42 +0200

    javascript en extra

--- VerticalSlider/script.js
+++ VerticalSlider/script.js
@@ -1,21 +1,25 @@
-const loadText = document.querySelector('.loading-text')
-const bg = document.querySelector('.bg')
-//functie voor het koppelen van blurren met de interval timeout
-const scale = (num, in_min, in_max, out_min, out_max) => {
-    return (num-in_min) * (out_max - out_min) / (in_max - in_min) + out_min
-}
+const sliderContainer = document.querySelector('.slider-container')
+const slideRight = document.querySelector('.right-slide')
+const slideLeft = document.querySelector('.left-slide')
+const upButton = document.querySelector('.up-button')
+const downButton = document.querySelector('.down-button')
+const slidesLength = slideRight.querySelectorAll('div').length
 
-let load = 0
+let activeSlideIndex = 0
 
-let int = setInterval(blurring, 30)
-function blurring() {
-    load++
+slideLeft.style.top = `-${(slidesLength - 1)* 100}vh`
 
-    if (load > 99) {
-        clearInterval(int)
+upButton.addEventListener('click', () => changeSlide('up'))
+downButton.addEventListener('click', () => changeSlide('down'))
+
+const changeSlide = (direction) => {
+    const sliderHeight = sliderContainer.clientHeight
+    if (direction == 'up') {
+        activeSlideIndex++
+        if(activeSlideIndex > slidesLenght - 1) {
+            activeSlideIndex = 0
+        }
     }
 
-    loadText.innerText = `${load}%`
-    loadText.style.opacity = scale(load, 0, 100, 1, 0)
-    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+    slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`
 }
