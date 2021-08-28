e3617fbfe9f07430d022cffb29c7700a097928e4
Wouter Dijks <wutu@Wouters-MacBook-Pro.local>
2021-08-28 19:31:11 +0200

    werkend project

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const loadText = document.querySelector('.loading-text')
 const bg = document.querySelector('.bg')
+//functie voor het koppelen van blurren met de interval timeout
 const scale = (num, in_min, in_max, out_min, out_max) => {
     return (num-in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 }
 
 let load = 0
 
-let int = setInterval(blurring, 60)
+let int = setInterval(blurring, 30)
 function blurring() {
     load++
 
@@ -16,4 +17,5 @@ function blurring() {
 
     loadText.innerText = `${load}%`
     loadText.style.opacity = scale(load, 0, 100, 1, 0)
+    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
 }
